6cd0b7453980aceeff8a4aa43264887c44af5c94
sbfrusho <rushocseru28@gmail.com>
2024-11-12 22:40:03 +0600

    all set

--- SmtpMailSending2.java
+++ SmtpMailSending2.java
@@ -5,60 +5,61 @@ import java.util.*;
 
 class SmtpMailSending2{
 
-    public static DataOutputStream dos;
-    public static BufferedReader br;
+   public static DataOutputStream dos;
+   public static BufferedReader br;
 
-    public static void main(String argv[]) throws Exception{
-        String user = "s2010776109@ru.ac.bd";
-        String pass = "b00cf74ff7";
+   public static void main(String argv[]) throws Exception{
+       String user = "s2010776109@ru.ac.bd";
+       String pass = "b00cf74ff7";
 
-        String username =new String(Base64.getEncoder().encode(user.getBytes()));
-        String password = new String(Base64.getEncoder().encode(pass.getBytes()));
-        SSLSocket s = (SSLSocket) SSLSocketFactory.getDefault().createSocket("smtp.gmail.com", 465);
-        dos = new DataOutputStream(s.getOutputStream());
-        br = new BufferedReader(new InputStreamReader(s.getInputStream()));
+       String username =new String(Base64.getEncoder().encode(user.getBytes()));
+       String password = new String(Base64.getEncoder().encode(pass.getBytes()));
+       
+       SSLSocket s = (SSLSocket) SSLSocketFactory.getDefault().createSocket("smtp.gmail.com", 465);
+       dos = new DataOutputStream(s.getOutputStream());
+       br = new BufferedReader(new InputStreamReader(s.getInputStream()));
 
-        send("EHLO smtp.gmail.com\r\n");
-        System.out.println("SERVER: "+ br.readLine());
-        System.out.println("SERVER: "+ br.readLine());
-        System.out.println("SERVER: "+ br.readLine());
-        System.out.println("SERVER: "+ br.readLine());
-        System.out.println("SERVER: "+ br.readLine());
-        System.out.println("SERVER: "+ br.readLine());
-        System.out.println("SERVER: "+ br.readLine());
-        System.out.println("SERVER: "+ br.readLine());
+       send("EHLO smtp.gmail.com\r\n");
+       // System.out.println("SERVER: "+ br.readLine());
+       // System.out.println("SERVER: "+ br.readLine());
+       // System.out.println("SERVER: "+ br.readLine());
+       // System.out.println("SERVER: "+ br.readLine());
+       // System.out.println("SERVER: "+ br.readLine());
+       // System.out.println("SERVER: "+ br.readLine());
+       // System.out.println("SERVER: "+ br.readLine());
+       // System.out.println("SERVER: "+ br.readLine());
 
-        send("AUTH LOGIN\r\n");
+       send("AUTH LOGIN\r\n");
 //        System.out.println("SERVER: "+ br.readLine());
 
-        send(username + "\r\n");
+       send(username + "\r\n");
 //        System.out.println("SERVER: "+ br.readLine());
 
-        send(password + "\r\n");
+       send(password + "\r\n");
 //        System.out.println("SERVER: "+ br.readLine());
 
-        send("MAIL FROM: <s2010776109@ru.ac.bd>\r\n");
+       send("MAIL FROM: <s2010776109@ru.ac.bd>\r\n");
 //        System.out.println("SERVER: "+ br.readLine());
 
-        send("RCPT TO: <sbfrusho@gmail.com>\r\n");
+       send("RCPT TO: <sbfrusho@gmail.com>\r\n");
 //        System.out.println("SERVER: "+ br.readLine());
 
-        send("DATA\r\n");
-        System.out.println("SERVER: "+ br.readLine());
+       send("DATA\r\n");
+       System.out.println("SERVER: "+ br.readLine());
 
-        send("FROM: s2010776109@ru.ac.bd\r\n");
-        send("TO: sbfrusho@gmail.com\r\n");
-        send("Subject: 2010776109" +  "\r\n");
-        send("ja khushi tai...vallagena.\r\n");
-        send(".\r\n");
+       send("FROM: s2010776109@ru.ac.bd\r\n");
+       send("TO: sbfrusho@gmail.com\r\n");
+       send("Subject: 2010776109" +  "\r\n");
+       send("ja khushi tai...vallagena.\r\n");
+       send(".\r\n");
 //        System.out.println("SERVER: "+ br.readLine());
 
 
-    }
+   }
 
-    private static void send(String s) throws Exception {
-        dos.writeBytes(s);
-        Thread.sleep(1000);
-        System.out.println("CLIENT: " + s);
-    }
+   private static void send(String s) throws Exception {
+       dos.writeBytes(s);
+       Thread.sleep(1000);
+       System.out.println("CLIENT: " + s);
+   }
 }
